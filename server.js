d50fdd58787ccdd9d38acd867878767fd4a9ffcd
DSP-Darkmoon <wony1390@gmail.com>
2026-04-05 10:58:00 +0900

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(express.json({ limit: '10mb' }));
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 // 정적 파일 서빙 - 카톡/외부 브라우저 호환을 위한 헤더 추가
-app.use(express.static('/app', {
+app.use(express.static(__dirname, {
   setHeaders: function(res, path) {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('X-Content-Type-Options', 'nosniff');
